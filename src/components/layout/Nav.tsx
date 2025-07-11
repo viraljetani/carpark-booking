a92b1e139e7a82c065cfd24b26f57932c553259c
Viral Jetani <viral.jetani@gmail.com>
2025-07-12 00:52:12 +1000

    nav links color update

--- src/components/layout/Nav.tsx
+++ src/components/layout/Nav.tsx
@@ -4,8 +4,8 @@ const Nav = () => {
   return (
     <nav className="flex justify-center">
       <ul className="flex space-x-4 items-center align-center">
-        <li><Link to="/booking">New Booking</Link></li>
-        <li><Link to="/bookings">Booking List</Link></li>
+        <li><Link to="/booking" className="text-indigo-900 hover:text-indigo-400" aria-current="page">New Booking</Link></li>
+        <li><Link to="/bookings" className="text-indigo-900 hover:text-indigo-400" aria-current="page">Booking List</Link></li>
       </ul>
     </nav>
   );
